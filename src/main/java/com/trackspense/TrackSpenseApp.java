4fb6d13543ee8c4bfed64249ce9089e244dd0c8c
rootikigai <ifykel360@gmail.com>
2025-09-14 22:44:57 +0100

    The application main

---
+++ src/main/java/com/trackspense/TrackSpenseApp.java
@@ -0,0 +1,11 @@
+package com.trackspense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackSpenseApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TrackSpenseApp.class, args);
+    }
+}
