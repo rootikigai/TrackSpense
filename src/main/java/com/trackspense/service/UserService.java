17dd6dc100234f575176d5525607c2c94da45f55
rootikigai <ifykel360@gmail.com>
2025-09-16 12:25:26 +0100

    Refactored to carry custom exceptions and validations

--- src/main/java/com/trackspense/service/UserService.java
+++ src/main/java/com/trackspense/service/UserService.java
@@ -2,10 +2,14 @@ package com.trackspense.service;
 
 import com.trackspense.data.model.User;
 import com.trackspense.data.repo.UserRepo;
+import com.trackspense.exception.InvalidEmailFormatException;
+import com.trackspense.exception.InvalidPasswordException;
+import com.trackspense.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +17,13 @@ public class UserService {
 
     private final UserRepo userRepo;
 
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
+
     public User registerUser(User user){
+        if(!EMAIL_PATTERN.matcher(user.getEmail()).matches()){
+            throw new InvalidEmailFormatException("Your Email no valid...check am!");
+        }
+
         Optional<User> existingUser = userRepo.findByEmail(user.getEmail());
         if (existingUser.isPresent()){
             throw new RuntimeException("Email is already registered...no vex");
@@ -22,21 +32,21 @@ public class UserService {
     }
 
     public User login(String email, String password){
-        Optional<User> existingUser = userRepo.findByEmail(email);
-        if (existingUser.isPresent()){
-            if (existingUser.get().getPassword().equals(password)){
-                return existingUser.get();
-            }
-            else{
-                throw new RuntimeException("Invalid password!");
-            }
+        if(!EMAIL_PATTERN.matcher(email).matches()){
+            throw new InvalidEmailFormatException("Your Email no valid...check am!");
         }
-        else {
-            throw new RuntimeException("User not found!");
+
+        User existingUser = userRepo.findByEmail(email).orElseThrow(() -> new UserNotFoundException("Dem no fit find am!"));
+        if (!existingUser.getPassword().equals(password)) {
+            throw new InvalidPasswordException("Your password no valid...check am!");
         }
+        return existingUser;
     }
 
-    public User findUserByEmail(String email){
-        return userRepo.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found"));
+    public User findUserByEmail(String email) {
+        if (!EMAIL_PATTERN.matcher(email).matches()) {
+            throw new InvalidEmailFormatException("Your Email no valid...check am!");
+        }
+        return userRepo.findByEmail(email).orElseThrow(() -> new UserNotFoundException("Dem no fit find am!"));
     }
 }
