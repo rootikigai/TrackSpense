f5a10a77b6d3621cccee5e81552a2b498b5ffe3d
rootikigai <ifykel360@gmail.com>
2025-09-17 06:05:35 +0100

    Refactored to be more secure.  Focused on user-scoped queries as every expense belongs to a user. Loose coupling in effect.  No method can accidentally fetch expense across all users.

--- src/main/java/com/trackspense/data/repos/ExpenseRepo.java
+++ src/main/java/com/trackspense/data/repos/ExpenseRepo.java
@@ -4,9 +4,11 @@ import com.trackspense.data.models.Category;
 import com.trackspense.data.models.Expense;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ExpenseRepo extends MongoRepository<Expense,String> {
     List<Expense> findByUserId(String userId);
-    List<Expense> findByCategory(Category category);
+    List<Expense> findByUserIdAndCategory(String userId, Category category);
+    List<Expense> findByUserIdAndDateBetween(String userId, LocalDateTime start, LocalDateTime end);
 }
