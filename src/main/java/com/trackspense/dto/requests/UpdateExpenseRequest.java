1aa6e4a2fd97e50a059c060b2ba60d97d7cf905e
rootikigai <ifykel360@gmail.com>
2025-09-18 20:17:43 +0100

    Added CreateExpenseRequest, UpdateExpenseRequest and ExpenseResponse DTOs

---
+++ src/main/java/com/trackspense/dto/requests/UpdateExpenseRequest.java
@@ -0,0 +1,16 @@
+package com.trackspense.dto.requests;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateExpenseRequest {
+    private double amount;
+    private String category;
+    private String description;
+    private LocalDateTime date;
+}
---
+++ src/main/java/com/trackspense/dto/responses/ExpenseResponse.java
@@ -0,0 +1,17 @@
+package com.trackspense.dto.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExpenseResponse {
+    private String id;
+    private double amount;
+    private String category;
+    private String description;
+    private LocalDateTime date;
+}
