0ae58eb72fe6e7b365c06b2c9808924a74368fad
rootikigai <ifykel360@gmail.com>
2025-09-20 23:56:44 +0100

    Added UserService Interface

--- src/main/java/com/trackspense/services/UserService.java
+++ src/main/java/com/trackspense/services/UserService.java
@@ -1,8 +1,10 @@
 package com.trackspense.services;
 
 import com.trackspense.dto.requests.RegisterUserRequest;
+import com.trackspense.dto.responses.LoginResponse;
 import com.trackspense.dto.responses.UserResponse;
 
 public interface UserService {
     UserResponse registerUser(RegisterUserRequest request);
+    LoginResponse login(String email, String password);
 }
