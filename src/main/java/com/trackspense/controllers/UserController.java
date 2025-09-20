de0287da45ee36ce7381081ff2e8ce9b0b389e4d
rootikigai <ifykel360@gmail.com>
2025-09-20 23:58:25 +0100

    Refactored UserController for proper endpoints

--- src/main/java/com/trackspense/controllers/UserController.java
+++ src/main/java/com/trackspense/controllers/UserController.java
@@ -6,11 +6,9 @@ import com.trackspense.dto.responses.LoginResponse;
 import com.trackspense.dto.responses.UserResponse;
 import com.trackspense.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -20,13 +18,13 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponse> registerUser(@RequestBody RegisterUserRequest request) {
-        UserResponse savedUser = userService.registerUser(request);
-        return ResponseEntity.ok(savedUser);
+    public ResponseEntity<UserResponse> register(@RequestBody RegisterUserRequest request) {
+        UserResponse response = userService.registerUser(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request){
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
         LoginResponse response = userService.login(request.getEmail(), request.getPassword());
         return ResponseEntity.ok(response);
     }
