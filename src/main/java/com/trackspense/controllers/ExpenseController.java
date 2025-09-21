81a5128b0aff14fa379470e6e1719c72367d5ab6
rootikigai <ifykel360@gmail.com>
2025-09-21 23:26:39 +0100

    updated to include userId in expense addition endpoint

--- src/main/java/com/trackspense/controllers/ExpenseController.java
+++ src/main/java/com/trackspense/controllers/ExpenseController.java
@@ -1,12 +1,12 @@
 package com.trackspense.controllers;
 
-import com.trackspense.data.models.Category;
+//import com.trackspense.data.models.Category;
 import com.trackspense.dto.requests.CreateExpenseRequest;
 import com.trackspense.dto.requests.UpdateExpenseRequest;
 import com.trackspense.dto.responses.ExpenseResponse;
-import com.trackspense.services.ExpenseService;
+import com.trackspense.services.ExpenseServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
+//import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,9 +18,9 @@ import java.util.List;
 @RequestMapping("/api/expenses")
 @RequiredArgsConstructor
 public class ExpenseController {
-    private final ExpenseService expenseService;
+    private final ExpenseServiceImpl expenseService;
 
-    @PostMapping("/add")
+    @PostMapping("/add/{userId}")
     public ResponseEntity<ExpenseResponse> addExpense(@PathVariable String userId, @RequestBody CreateExpenseRequest request){
         ExpenseResponse savedExpense = expenseService.addExpense(request, userId);
         return ResponseEntity.ok(savedExpense);
@@ -50,27 +50,27 @@ public class ExpenseController {
         return ResponseEntity.ok(list);
     }
 
-    @GetMapping("/user/{userId}/category/{category}")
-    public ResponseEntity<List<ExpenseResponse>> getExpensesByUserAndCategory(
-            @PathVariable String userId,
-            @PathVariable String category) {
-        List<ExpenseResponse> list = expenseService.getExpensesByUserAndCategory(userId, category);
-        return ResponseEntity.ok(list);
-    }
-
-    @GetMapping("/user/{userId}/date")
-    public ResponseEntity<List<ExpenseResponse>> getExpensesByUserAndDateRange(
-            @PathVariable String userId,
-            @RequestParam("start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start,
-            @RequestParam("end")   @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime end) {
-
-        List<ExpenseResponse> list = expenseService.getExpensesByUserAndDateRange(userId, start, end);
-        return ResponseEntity.ok(list);
-    }
-
-    @GetMapping("/categories")
-    public ResponseEntity<List<String>> getAllCategories(){
-        List<String> categories = Arrays.stream(Category.values()).map(Enum::name).toList();
-        return ResponseEntity.ok(categories);
-    }
+//    @GetMapping("/user/{userId}/category/{category}")
+//    public ResponseEntity<List<ExpenseResponse>> getExpensesByUserAndCategory(
+//            @PathVariable String userId,
+//            @PathVariable String category) {
+//        List<ExpenseResponse> list = expenseService.getExpensesByUserAndCategory(userId, category);
+//        return ResponseEntity.ok(list);
+//    }
+//
+//    @GetMapping("/user/{userId}/date")
+//    public ResponseEntity<List<ExpenseResponse>> getExpensesByUserAndDateRange(
+//            @PathVariable String userId,
+//            @RequestParam("start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start,
+//            @RequestParam("end")   @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime end) {
+//
+//        List<ExpenseResponse> list = expenseService.getExpensesByUserAndDateRange(userId, start, end);
+//        return ResponseEntity.ok(list);
+//    }
+//
+//    @GetMapping("/categories")
+//    public ResponseEntity<List<String>> getAllCategories(){
+//        List<String> categories = Arrays.stream(Category.values()).map(Enum::name).toList();
+//        return ResponseEntity.ok(categories);
+//    }
 }
