19364101e96bb0e20a1d6b23cbf448b1867648b4
rootikigai <ifykel360@gmail.com>
2025-09-22 19:49:03 +0100

    unpaused find by user with date and/or category

--- src/main/java/com/trackspense/data/repos/ExpenseRepo.java
+++ src/main/java/com/trackspense/data/repos/ExpenseRepo.java
@@ -1,14 +1,14 @@
 package com.trackspense.data.repos;
 
-//import com.trackspense.data.models.Category;
+import com.trackspense.data.models.Category;
 import com.trackspense.data.models.Expense;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-//import java.time.LocalDateTime;
+import java.time.LocalDateTime;
 import java.util.List;
 
-public interface ExpenseRepo extends MongoRepository<Expense,String> {
+public interface ExpenseRepo extends MongoRepository<Expense, String> {
     List<Expense> findByUserId(String userId);
-//    List<Expense> findByUserIdAndCategory(String userId, Category category);
-//    List<Expense> findByUserIdAndDateBetween(String userId, LocalDateTime start, LocalDateTime end);
+    List<Expense> findByUserIdAndCategory(String userId, Category category);
+    List<Expense> findByUserIdAndDateBetween(String userId, LocalDateTime start, LocalDateTime end);
 }
