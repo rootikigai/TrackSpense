e72ea3db3435fec8824d2ac54bb80a78e067575e
rootikigai <ifykel360@gmail.com>
2025-09-22 19:59:21 +0100

    Added ExpenseService interface

---
+++ src/main/java/com/trackspense/services/ExpenseService.java
@@ -0,0 +1,25 @@
+package com.trackspense.services;
+
+import com.trackspense.data.models.Category;
+import com.trackspense.dto.requests.CreateExpenseRequest;
+import com.trackspense.dto.requests.UpdateExpenseRequest;
+import com.trackspense.dto.responses.ExpenseResponse;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface ExpenseService {
+    ExpenseResponse addExpense(CreateExpenseRequest request, String userId);
+
+    ExpenseResponse updateExpense(String userId, String expenseId, UpdateExpenseRequest request);
+
+    void deleteExpense(String userId, String expenseId);
+
+    List<ExpenseResponse> getAllExpenses(String userId);
+
+    List<ExpenseResponse> getExpensesByUser(String userId);
+
+    List<ExpenseResponse> getExpensesByUserAndCategory(String userId, Category category);
+
+    List<ExpenseResponse> getExpensesByUserAndDateRange(String userId, LocalDateTime start, LocalDateTime end);
+}
