b0f3442d9ead8e183ad16ce88a82145e0d618420
rootikigai <ifykel360@gmail.com>
2025-09-28 16:32:07 +0100

    Refactored cors config file allowedOrigins

--- src/main/java/com/trackspense/config/CorsConfig.java
+++ src/main/java/com/trackspense/config/CorsConfig.java
@@ -13,8 +13,11 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // apply to all endpoints
-                        .allowedOrigins("http://localhost:5500") // your frontend URL
+                registry.addMapping("/**")
+                        .allowedOrigins( "http://127.0.0.1:5500",
+                                "http://localhost:5500",
+                                "http://localhost:3000",
+                                "http://localhost:5173" )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
