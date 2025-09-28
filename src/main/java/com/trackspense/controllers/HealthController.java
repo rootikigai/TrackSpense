a599aba53b98d8f1ab2790dd810afe0f308d497b
rootikigai <ifykel360@gmail.com>
2025-09-28 17:33:32 +0100

    Health controller added to test if apis hit targets on the console.

---
+++ src/main/java/com/trackspense/controllers/HealthController.java
@@ -0,0 +1,21 @@
+package com.trackspense.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HealthController {
+
+    @GetMapping({"", "/"})
+    public ResponseEntity<Void> root() {
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<Void> health() {
+        return ResponseEntity.noContent().build();
+    }
+}
