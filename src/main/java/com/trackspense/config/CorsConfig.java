fff3f01b4e9abf26d11ed7053e04fca1ba1aef8d
rootikigai <ifykel360@gmail.com>
2025-12-03 23:13:11 +0100

    Minor update

--- src/main/java/com/trackspense/config/CorsConfig.java
+++ src/main/java/com/trackspense/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.trackspense.config;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
